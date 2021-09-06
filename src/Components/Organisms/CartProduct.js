ccd1fc52f246c9a264e0472b2eb49e582406f268
pepelias <j.avellogomez@gmail.com>
2021-09-06 06:07:53 -0300

    Confirmar quitar del carrito

--- src/Components/Organisms/CartProduct.js
+++ src/Components/Organisms/CartProduct.js
@@ -1,20 +1,31 @@
+import { useRef } from 'react'
 import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { Currency } from '../../helpers/numberFormat'
 import { removeFromCart } from '../../redux/actionCreators'
 import Icon from '../Molecules/Icon'
 
 const CartProduct = ({ amount, price, quantity, product }) => {
   const dispatch = useDispatch()
+  const element = useRef()
   const handleRemove = () => {
+    element.current.classList.toggle('remove')
+  }
+  const dispatchRemove = () => {
     dispatch(removeFromCart(product.handle))
   }
+  const link = `/productos/${product.handle}`
   return (
-    <div className="cart-product">
+    <div className="cart-product" ref={element}>
       <div className="cart-product__image">
-        <img src={product.imageSrc} alt={product.title} />
+        <Link to={link}>
+          <img src={product.imageSrc} alt={product.title} />
+        </Link>
       </div>
       <div className="cart-product__info">
-        <h1 className="cart-product__title">{product.title}</h1>
+        <Link to={link}>
+          <h1 className="cart-product__title">{product.title}</h1>
+        </Link>
         <p>Precio unidad: {price}</p>
         <p>Cantidad: {quantity}</p>
         <p>
@@ -25,6 +36,17 @@ const CartProduct = ({ amount, price, quantity, product }) => {
         <button className="icon" onClick={handleRemove}>
           <Icon icon="trash-can" />
         </button>
+      </div>
+      <div className="cart-product__remove-banner">
+        <h2>¿Desea quitar este producto?</h2>
+        <div className="buttons-container to-end">
+          <button onClick={dispatchRemove}>
+            <Icon icon="check-mark">Confirmar</Icon>
+          </button>
+          <button className="secondary" onClick={handleRemove}>
+            <Icon icon="x-mark">Cancelar</Icon>
+          </button>
+        </div>
       </div>
     </div>
   )
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -4,9 +4,9 @@ import CartProduct from '../Components/Organisms/CartProduct'
 const useCart = () => {
   const cart = useSelector(({ cart }) => cart)
   let total = 0
-  const products = cart.map((p, i) => {
+  const products = cart.map((p) => {
     total = total + p.amount * p.quantity
-    return <CartProduct key={i} {...p} />
+    return <CartProduct key={p.product.handle} {...p} />
   })
 
   return { products, total }
