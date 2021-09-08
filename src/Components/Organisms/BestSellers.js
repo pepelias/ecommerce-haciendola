d3286046053e4c0ac0685edc8739c4b6f8f29c1b
pepelias <j.avellogomez@gmail.com>
2021-09-08 02:27:32 -0300

    ajustar z-index apropiadamente

--- src/Components/Organisms/BestSellers.js
+++ src/Components/Organisms/BestSellers.js
@@ -15,7 +15,7 @@ const BestSellers = () => {
     { width: 0, itemsToShow: 1, itemsToScroll: 1 },
     { width: 500, itemsToShow: 2, itemsToScroll: 1 },
     { width: 768, itemsToShow: 3, itemsToScroll: 2 },
-    { width: 1200, itemsToShow: 4, itemsToScroll: 1 },
+    { width: 1200, itemsToShow: 4, itemsToScroll: 2 },
   ]
 
   const ended = ({ index }) => {
